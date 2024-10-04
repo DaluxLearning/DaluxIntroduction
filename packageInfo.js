a441c2df0fa1ad7cde8341eb845ad9911059647e
AndersHLDalux <163159372+AndersHLDalux@users.noreply.github.com>
2024-10-04 15:18:48 +0200

    Version5

--- packageInfo.js
+++ packageInfo.js
@@ -1,1 +1,1 @@
-var DKI_Package_Info = {"date":"October, 03 2024 06:23:39","publishing_profile_id":"D1BE8848-B35E-62B1-011B-A0B3D8C46949","version":"7.4.4.1.0","claro_url":"https://app.authr.it/"};
+var DKI_Package_Info = {"date":"October, 04 2024 09:17:15","publishing_profile_id":"D1BE8848-B35E-62B1-011B-A0B3D8C46949","version":"7.4.4.1.0","claro_url":"https://app.authr.it/"};
