fca5acdaaab76ad8a9ac8552f43dd5ad86108ecd
AndersHLDalux <163159372+AndersHLDalux@users.noreply.github.com>
2024-10-28 15:15:25 +0100

    Infra2

--- packageInfo.js
+++ packageInfo.js
@@ -1,1 +1,1 @@
-var DKI_Package_Info = {"date":"October, 28 2024 09:44:11","publishing_profile_id":"D1BE8848-B35E-62B1-011B-A0B3D8C46949","version":"7.4.4.2.1","claro_url":"https://app.authr.it/"};
+var DKI_Package_Info = {"date":"October, 28 2024 10:13:25","publishing_profile_id":"D1BE8848-B35E-62B1-011B-A0B3D8C46949","version":"7.4.4.2.1","claro_url":"https://app.authr.it/"};
