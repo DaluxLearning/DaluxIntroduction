0e1928ded225cd9b7c22b829fbd1575ce5406c80
AndersHLDalux <163159372+AndersHLDalux@users.noreply.github.com>
2024-11-29 13:43:35 +0100

    Clickable links

--- packageInfo.js
+++ packageInfo.js
@@ -1,1 +1,1 @@
-var DKI_Package_Info = {"date":"November, 29 2024 07:28:54","publishing_profile_id":"D1BE8848-B35E-62B1-011B-A0B3D8C46949","version":"7.4.4.3.1","claro_url":"https://app.authr.it/"};
+var DKI_Package_Info = {"date":"November, 29 2024 07:40:21","publishing_profile_id":"D1BE8848-B35E-62B1-011B-A0B3D8C46949","version":"7.4.4.3.1","claro_url":"https://app.authr.it/"};
